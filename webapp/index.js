6f78024ab880e3c0a3581a5b19b0f0407f8fe25b
Matthis <foulonneau.matthis@gmail.com>
2023-01-07 21:07:31 +0100

    Changed Repo Dir Structure

--- webapp/index.js
+++ webapp/index.js
@@ -1,45 +1,73 @@
 //Create web app using pug that calls the /qr/real endpoint
 
 const express = require('express');
-const ejs = require('ejs');
-const request = require('request');
-const bodyParser = require('body-parser');
+const http = require('http');
+const path = require("path");
+
 const app = express();
 const port = 8080;
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+async function getFakeQR() {
+  return new Promise((resolve, reject) => {
+    http.get('http://localhost:3000/qr', (resp) => {
+      let data = '';
 
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      resp.on('end', () => {
+        resolve(data);
+      });
+
+    }).on("error", (err) => {
+      reject(err);
+    });
+  });
+}
+
+async function getRealQR(url) {
+    return new Promise((resolve, reject) => {
+      http.get('http://localhost:3000/qr/real?url='+url, (resp) => {
+        let data = '';
+  
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+  
+        resp.on('end', () => {
+          resolve(data);
+        });
+  
+      }).on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+  
+
+
+
+app.set('view engine', 'pug');
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('./'))
 
 app.get("/", (req, res) => {
-    res.render("index", { title: "Mdrlol" ,userprofile: "Matthis Foulonneau" });
-  });
+    res.render("index", { title: "Home" });
+});
+app.get("/result", async (req, res) => {
+    // Get text variables inside url
+    const url = req.query.url;
+    // wait for async function to finish
+    const fakeqr = await getFakeQR();
+    const realqr = await getRealQR(url);
 
-app.get("/qr/real", (req, res) => {
-  request('http://localhost:3000/qr/real?url='+req.query.url, function (error, response, body) {
-    if (!error) {
-      console.log(req.query.url); 
-      res.render("realqr", { title: "Mdrlol" ,qr: body, url: req.query.url });
-    }
-  });
+    // Render result page
+    res.render("result", { title: "QR", fake:fakeqr, real:realqr});
 });
 
-app.get("/qr", (req, res) => {
-  request('http://localhost:3000/qr', function (error, response, body) {
-    if (!error) {
-      console.log(req.query.url); 
-      res.render("qr", { title: "Mdrlol" ,qr: body, url: req.query.url });
-    }
-  });
-});
-
-
-
+console.log("Starting webapp");
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Server listening at http://localhost:${port}`);
 });
-  
